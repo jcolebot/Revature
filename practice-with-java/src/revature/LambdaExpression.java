86dc931b263602a1701e91285752c8f3f327d42f
jcolebot <jcthewriter@gmail.com>
2021-10-27 13:03:20 -0400

    New practice programs

--- practice-with-java/src/revature/LambdaExpression.java
+++ practice-with-java/src/revature/LambdaExpression.java
@@ -1,4 +1,22 @@
 package revature;
+import java.util.ArrayList;
+
+// Lambda expression is a short block of code that takes parameters and returns a value
+// Similar to methods, but do not need named and can be used in the body of a method
+// Syntax examples:
+// parameter -> expression
+// (parameter1, parameter2) -> expression
+// (parameter1, parameter2) -> {block of code}
 
 public class LambdaExpression {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(9);
+        numbers.add(8);
+        numbers.add(7);
+        numbers.forEach((n) -> {System.out.println(n);});
+    }
 }
